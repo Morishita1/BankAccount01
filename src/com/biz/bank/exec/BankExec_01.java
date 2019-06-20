acf51dc095244f7f1bc3999db4d839efc633d7d5
Morishtia1 <iakwpi@naver.com>
2019-06-20 16:42:43 +0900

    first

--- src/com/biz/bank/exec/BankExec_01.java
+++ src/com/biz/bank/exec/BankExec_01.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 import com.biz.bank.model.BankBalanceVO;
 import com.biz.bank.service.BankService;
+import com.biz.bank.service.BankServiceimp_02;
 
 public class BankExec_01 {
 
@@ -16,9 +17,13 @@ public class BankExec_01 {
 		String fileName="src/com/biz/bank/balance.txt";
 		// bs 선언부만 try 밖으로 빼내고
 		// null값으로 초기화를 한다
+		
+		// interface가 있는 클래스는
+		// 선언부에서는 interface로 선언을 한다.
+		//      초기화 생성은 그 interface를 imp 한 클래스로 실행을 한다.
 		BankService bs=null;
 		try {
-			bs=new BankService(fileName);
+			bs=new BankServiceimp_02(fileName);
 			bs.readBalance();
 
 		} catch (FileNotFoundException e) {
@@ -32,20 +37,33 @@ public class BankExec_01 {
 			
 			
 			int intMenu=bs.selectMenu();
-			if(intMenu==-9)break;
+			if(intMenu==-9) {
+				try {
+					bs.writeBalance();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			}
 			System.out.print("계좌번호>>");
 			String accNum=scan.nextLine();
 			
+			BankBalanceVO vo=bs.pickAcc(accNum);
+			if(vo==null) {
+				System.out.println("계좌번호가 없습니다!!");
+				continue;
+			}
 			// 입금처리
 			if(intMenu==1) {
-				System.out.println("입금금액>>");
+				System.out.print("입금금액>>");
 				String strInputMoney=scan.nextLine();
 				int intInputMoney=Integer.valueOf(strInputMoney);
 				bs.inputMoney(accNum, intInputMoney);
 			};
 			// 출금처리
 			if(intMenu==2) {
-				System.out.println("출금금액>>");
+				System.out.print("출금금액>>");
 				String strOutputMoney=scan.nextLine();
 				int intOutputMoney=Integer.valueOf(strOutputMoney);
 				bs.outputMoney(accNum, intOutputMoney);
